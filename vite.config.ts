f052d3dd0be08cc4fc27bc97be331275eae8676a
Julian Rodriguez <112611772+julianfroes@users.noreply.github.com>
2025-05-11 16:22:57 -0700

    fix 2 del config y package

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:"/presupuestos",
+  base:"/presupuestos/",
+  build: {
+    outDir: 'dist', // Vite usa 'dist' en lugar de 'build'
+    emptyOutDir: true,
+  }
 })
